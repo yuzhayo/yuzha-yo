777ec320d90c90e30d67da7d1d928d45b7dd1db5
dhepilinuz <dhepilinuz@gmail.com>
2025-09-24 02:36:23 +0700

    .

--- merge/0_Implementasi/yuzha/src/App.tsx
+++ merge/0_Implementasi/yuzha/src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import MainScreen from "./MainScreen";
+﻿import React from 'react'
+import LauncherScreen from './launcher/LauncherScreen'
 
-const App: React.FC = () => <MainScreen />;
+const App: React.FC = () => <LauncherScreen />
 
-export default App;
+export default App
