d0aa35eeadd926990413ac051c3b7f010186ac44
dhepilinuz <dhepilinuz@gmail.com>
2025-10-22 02:38:04 +0700

    .

--- yuzha/src/test/testscreen.tsx
+++ yuzha/src/test/testscreen.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import bgArtifactCraftCenter from "@shared/asset/BG_ArtifactCraft_Center.png";
 import gearMoon from "@shared/asset/GEARMOON.png";
 import lockIcon from "@shared/asset/V3_LockIcon.png";
+import hourHand from "@shared/asset/H12.png";
 
 export type TestScreenProps = {
   onBack?: () => void;
@@ -12,6 +13,9 @@ export default function TestScreen(props: TestScreenProps) {
   const gearOrbitDurationSeconds = 20;
   const lockOrbitRadiusPx = 300;
   const lockOrbitDurationSeconds = 32;
+  const hourOrbitRadiusPx = 240;
+  const hourOrbitDurationSeconds = 48;
+  const hourSpinDurationSeconds = 12;
 
   return (
     <div className="relative min-h-screen bg-slate-950 text-white">
@@ -21,6 +25,42 @@ export default function TestScreen(props: TestScreenProps) {
           alt="Artifact Craft center background"
           className="h-full w-full animate-[spin_72s_linear_infinite] object-contain"
         />
+      </div>
+      <div
+        className="pointer-events-none absolute left-1/2 top-1/2 z-30"
+        style={{
+          width: hourOrbitRadiusPx * 2,
+          height: hourOrbitRadiusPx * 2,
+          marginLeft: -hourOrbitRadiusPx,
+          marginTop: -hourOrbitRadiusPx,
+        }}
+      >
+        <div
+          className="relative h-full w-full"
+          style={{
+            animation: `orbit ${hourOrbitDurationSeconds}s linear infinite`,
+            transformOrigin: "50% 50%",
+          }}
+        >
+          <div
+            style={{
+              position: "absolute",
+              left: "50%",
+              top: "50%",
+              transform: `translate(-50%, -50%) translateY(-${hourOrbitRadiusPx}px)`,
+            }}
+          >
+            <img
+              src={hourHand}
+              alt="Orbiting hour hand"
+              style={{
+                display: "block",
+                transformOrigin: "50% 50%",
+                animation: `spin ${hourSpinDurationSeconds}s linear infinite`,
+              }}
+            />
+          </div>
+        </div>
       </div>
       <div
         className="pointer-events-none absolute left-1/2 top-1/2 z-20"
