352435f18d8899e2d9ed8fd955de7593cc04ef98
dhepilinuz <dhepilinuz@gmail.com>
2025-10-22 02:23:14 +0700

    .

--- yuzha/src/test/testscreen.tsx
+++ yuzha/src/test/testscreen.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import bgArtifactCraftCenter from "@shared/asset/BG_ArtifactCraft_Center.png";
+import gearMoon from "@shared/asset/GEARMOON.png";
 
 export type TestScreenProps = {
   onBack?: () => void;
 };
 
 export default function TestScreen(props: TestScreenProps) {
+  const orbitDurationSeconds = 3600;
+  const spinDurationSeconds = 72;
+
   return (
     <div className="relative min-h-screen bg-slate-950 text-white">
       <div className="absolute left-1/2 top-1/2 h-1/2 w-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -14,6 +18,21 @@ export default function TestScreen(props: TestScreenProps) {
           alt="Artifact Craft center background"
           className="h-full w-full animate-[spin_72s_linear_infinite] object-contain"
         />
+      </div>
+      <div className="pointer-events-none absolute left-1/2 top-1/2 z-20 h-[60vmin] w-[60vmin] -translate-x-1/2 -translate-y-1/2">
+        <div
+          className="absolute left-1/2 top-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2 animate-spin"
+          style={{ animationDuration: `${orbitDurationSeconds}s` }}
+        >
+          <div className="absolute left-1/2 top-0 -translate-x-1/2">
+            <img
+              src={gearMoon}
+              alt="Orbiting gear moon"
+              className="h-20 w-20 object-contain animate-spin"
+              style={{ animationDuration: `${spinDurationSeconds}s` }}
+            />
+          </div>
+        </div>
       </div>
       {props.onBack && (
         <button
